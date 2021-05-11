dd1866a4f2ead295ef3fc740cc42bda823676e53
Leonardo Golfeto <36703898+leonardogolfeto@users.noreply.github.com>
2021-05-11 09:48:56 -0300

    Add files via upload

---
+++ scripts/setup.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (id,separador_milhar,separador_decimal,senha,is_inversao_paridade_cambial,is_aplica_na_transacao,id_cambio_referencia,is_atualizacao_automatica,is_inverte_passivo,id_conta_contabil_cta,converte_apresentacao) VALUES
+	 (786,'.',',',1036,false,false,NULL,true,false,1569622,false);
